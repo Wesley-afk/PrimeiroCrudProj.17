2befda31443d94e03c4a36f79560eea0e9cc8381
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2024-10-19 11:27:13 -0300

    Add files via upload

---
+++ Projeto17_ CRUD/Script_Proj17.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE teste;
+USE teste;
+
+CREATE TABLE flauta(
+ID_flauta INT PRIMARY KEY,
+ano_fabricacao int,
+Marca VARCHAR(45)
+); 
+
+SELECT * FROM flauta;
---
+++ Projeto17_ CRUD/Wesley_Crud/src/main/java/com/mycompany/projeto_teste/Projeto_Teste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_teste;
+
+/**
+ *
+ * @author w.rocha
+ */
+public class Projeto_Teste {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
